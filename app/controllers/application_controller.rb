fef637ac3342026d5a8ae929c414bc71607fb0a1
Hayden Betts <haydenbetts@gmail.com>
2017-09-29 01:45:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
 def new
 end
 
+def create
+  Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+end
+
 end
